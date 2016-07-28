bde53942ddb1ade16e88f74977032fafb7ab2a28
triveni badgujar <triveni@forusall.com>
2016-07-28 18:08:34 +0530

    Login page with basic css

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './js/store';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
 
 // ReactDOM.render(
@@ -16,12 +17,14 @@ import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
 
 ReactDOM.render((
 	<Provider store={store}>
+	 <MuiThemeProvider>
 	   <Router history = {browserHistory}>
 	      <Route path = "/" component = {Login}>
 	         <IndexRoute component = {Main} />
 	         <Route path = "home" component = {Main} />
 	      </Route>
 	   </Router>
+	 </MuiThemeProvider>
 	</Provider>
 ), document.getElementById('root'))
 
--- src/js/component/app/Login.js
+++ src/js/component/app/Login.js
@@ -1,35 +1,52 @@
 import React from 'react';
-
+import { Push } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../../actions/eventActions';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
-
-function mapStateToProps(state){
-	return {
-		
-	};
-}
-
-function mapDispatchToProps(dispatch){
-	return {
-		actions: bindActionCreators(actionCreators, dispatch)
-	};
-}
  
-class Login extends React.Component {
-	render(){
-		return (
-			<div>
-
-				Login Form
-				<ul>
-					<Link to="home" >See Events</Link>
-				</ul>
-			</div>
-		);
-
-	}
+export default class Login extends React.Component{
+    handleOnClick () {
+        this.props.history.push(null,"Main");
+    }
+    render(){
+        return(
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4 col-md-offset-4">
+                            <div className="login-panel panel panel-default">
+                                <div className="panel-heading">
+                                    <h3 className="panel-title">Please Sign In</h3>
+                                </div>
+                                <div className="panel-body">
+                                    <form>
+                                        <fieldset>
+                                            <div className="form-group">
+                                                <input type="email" className="form-control" placeholder="E-mail" name="email"/>
+                                            </div>
+                                            <div className="form-group">
+                                                <input type="password" className="form-control" placeholder="Password" name="password" value=""/>
+                                            </div>
+                                            <button className="btn btn-lg btn-success.btn-outline btn-block" onClick={this.handleOnClick.bind(this)}></button>
+                                        </fieldset>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+/*const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    //routeDispatch: dispatch
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);*/
