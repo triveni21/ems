cd46be3a1dd5adcd13af6fc0c15756369d963b2b
Rajashri Udhoji <rajashriu@synerzipune.local>
2016-09-28 18:23:33 +0530

    Fix Event Details Layout

--- src/js/component/app/EventDetails.js
+++ src/js/component/app/EventDetails.js
@@ -21,61 +21,51 @@ class EventDetails extends  React.Component{
 
     }
     render(){
+        debugger;
         let eventDetails = _.map(this.props.data,(option) => {
-            if(option.category_id == this.props.category_id){
+            if(!this.props.eventSelected && (option.id == this.props.eventSelected)){
                 return(
-                    <table>
-                        <tr>
-                            <td>
-                                <div><p style={{color:'#00BCD4',fontSize:'15px', fontWeight:'900', fontStretch:'ultra-expanded', fontStyle: 'oblique'}}>Event Name :</p></div>
-                            </td>
-                            <td>
-                                <div>{option.title}</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div><p style={{color:'#00BCD4',fontSize:'15px', fontWeight:'900', fontStretch:'ultra-expanded', fontStyle: 'oblique'}}> Event Start Date:</p></div>
-                            </td>
-                            <td>
-                                <div>{option.start_date}</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div> <p style={{color:'#00BCD4',fontSize:'15px', fontWeight:'900', fontStretch:'ultra-expanded', fontStyle: 'oblique'}}> Event End Date:</p></div>
-                            </td>
-                            <td>
-                                <div>{option.end_date}</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div> <p style={{color:'#00BCD4',fontSize:'15px', fontWeight:'900', fontStretch:'ultra-expanded', fontStyle: 'oblique'}}>Event Description:</p></div>
-                            </td>
-                            <td>
-                                <div>{option.description}</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div><p style={{color:'#00BCD4',fontSize:'15px', fontWeight:'900', fontStretch:'ultra-expanded', fontStyle: 'oblique'}}>Event Type:</p></div>
-                            </td>
-                            <td>
-                                <div>{this.props.eventFlag}</div>
-                            </td>
-                        </tr>
-                    </table>
-                );
-            }
-        });
-        return(
-            <div>
-                <Paper style={style} zDepth={2}>
-                    <div style={{textAlign:'center',padding:'40px'}}>
-                        {eventDetails}
+                    <div>
                         <table>
                             <tr>
+                                <td>
+                                    <div><p style={{color:'#00BCD4',fontSize:'15px', fontWeight:'900', fontStretch:'ultra-expanded', fontStyle: 'oblique'}}>Event Name :</p></div>
+                                </td>
+                                <td>
+                                    <div>{option.title}</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div><p style={{color:'#00BCD4',fontSize:'15px', fontWeight:'900', fontStretch:'ultra-expanded', fontStyle: 'oblique'}}> Event Start Date:</p></div>
+                                </td>
+                                <td>
+                                    <div>{option.start_date}</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div> <p style={{color:'#00BCD4',fontSize:'15px', fontWeight:'900', fontStretch:'ultra-expanded', fontStyle: 'oblique'}}> Event End Date:</p></div>
+                                </td>
+                                <td>
+                                    <div>{option.end_date}</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div> <p style={{color:'#00BCD4',fontSize:'15px', fontWeight:'900', fontStretch:'ultra-expanded', fontStyle: 'oblique'}}>Event Description:</p></div>
+                                </td>
+                                <td>
+                                    <div>{option.description}</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div><p style={{color:'#00BCD4',fontSize:'15px', fontWeight:'900', fontStretch:'ultra-expanded', fontStyle: 'oblique'}}>Event Type:</p></div>
+                                </td>
+                                <td>
+                                    <div>{this.props.eventFlag}</div>
+                                </td>
                             </tr>
                         </table>
                         <table>
@@ -84,13 +74,35 @@ class EventDetails extends  React.Component{
                             </tr>
                         </table>
                     </div>
-                </Paper>
-            </div>
+                );
+            }
+            /*if(option.category_id == this.props.category_id){
+
+            }*/
+        });
+        if(this.props.eventSelected){
+            return(
+                <div>
+                    <Paper style={style} zDepth={2}>
+                        <div style={{textAlign:'center',padding:'40px'}}>
+                            {eventDetails}
+                        </div>
+                    </Paper>
+                </div>
+            );
+        }
+        return (
+            <Paper style={style} zDepth={2}>
+                <div style={{color:'#CCCCCC',fontSize:30,paddingTop:200, width:1000,float:'center'}}>
+                    Select Event to View or Edit the Event.
+                </div>
+            </Paper>
         );
     }
 }
 const mapStateToProps = (state) => ({
     data: state.events["events"],
+    eventSelected : state.eventSelected
 });
 
 const mapDispatchToProps = (dispatch) => ({
